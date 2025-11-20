0e583939274710ab6b831e7dc65d2e3c4d944eab
Zhaqi07 <muhammadzhaqi461@gmail.com>
2025-11-20 17:36:30 +0700

    insert dan delete parent, child, relation

--- main.cpp
+++ main.cpp
@@ -2,7 +2,128 @@
 
 using namespace std;
 
+struct dataKelas
+{
+    string id_Kelas;
+    string nama_kelas;
+    string jadwal_kelas;
+    int kapasitas_kelas;
+    int biaya_kelas;
+    string nama_coach;
+    dataKelas* next;
+};
+
+struct dataPeserta
+{
+    string id_Peserta;
+    string nama_peserta;
+    dataPeserta* next;
+};
+
+struct dataKehadiran
+{
+    string tanggal_registrasi;
+    string status_kehadiran;
+    string presensi;
+    int rating;
+    string catatan;
+    dataKehadiran* next;
+    dataPeserta* peserta;
+    dataKelas* kelas;
+};
+
+void insertDataKelas(dataKelas** head, const string& id, const string& nama, const string& jadwal, int kapasitas, int biaya, const string& coach) {
+    dataKelas* newKelas = new dataKelas();
+    newKelas->id_Kelas = id;
+    newKelas->nama_kelas = nama;
+    newKelas->jadwal_kelas = jadwal;
+    newKelas->kapasitas_kelas = kapasitas;
+    newKelas->biaya_kelas = biaya;
+    newKelas->nama_coach = coach;
+    newKelas->next = *head;
+    *head = newKelas;
+}
+
+void insertDataPeserta(dataPeserta** head, const string& id, const string& nama) {
+    dataPeserta* newPeserta = new dataPeserta();
+    newPeserta->id_Peserta = id;
+    newPeserta->nama_peserta = nama;
+    newPeserta->next = *head;
+    *head = newPeserta;
+}
+
+void insertDataKehadiran(dataKehadiran** head, const string& tanggal, const string& status, const string& presensi, int rating, const string& catatan, dataPeserta* peserta, dataKelas* kelas) {
+    dataKehadiran* newKehadiran = new dataKehadiran();
+    newKehadiran->tanggal_registrasi = tanggal;
+    newKehadiran->status_kehadiran = status;
+    newKehadiran->presensi = presensi;
+    newKehadiran->rating = rating;
+    newKehadiran->catatan = catatan;
+    newKehadiran->peserta = peserta;
+    newKehadiran->kelas = kelas;
+    newKehadiran->next = *head;
+    *head = newKehadiran;
+}
+
+void deleteDataKelas(dataKelas** head, const string& id) {
+    dataKelas* temp = *head;
+    dataKelas* prev = nullptr;
+
+    while (temp != nullptr && temp->id_Kelas != id) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) return; // Not found
+
+    if (prev == nullptr) {
+        *head = temp->next; // Delete head
+    } else {
+        prev->next = temp->next; // Bypass the node to be deleted
+    }
+    delete temp;
+}
+
+void deleteDataPeserta(dataPeserta** head, const string& id) {
+    dataPeserta* temp = *head;
+    dataPeserta* prev = nullptr;
+
+    while (temp != nullptr && temp->id_Peserta != id) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) return; // Not found
+
+    if (prev == nullptr) {
+        *head = temp->next; // Delete head
+    } else {
+        prev->next = temp->next; // Bypass the node to be deleted
+    }
+    delete temp;
+}
+
+void deleteDataKehadiran(dataKehadiran** head, const string& tanggal) {
+    dataKehadiran* temp = *head;
+    dataKehadiran* prev = nullptr;
+
+    while (temp != nullptr && temp->tanggal_registrasi != tanggal) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == nullptr) return; // Not found
+
+    if (prev == nullptr) {
+        *head = temp->next; // Delete head
+    } else {
+        prev->next = temp->next; // Bypass the node to be deleted
+    }
+    delete temp;
+}
+
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
 }
+
