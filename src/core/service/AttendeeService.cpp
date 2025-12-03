32b13b53ebd5fc91d3b5a209ee389c065fc1429c
bodybody <bodybodyofficial@gmail.com>
2025-12-03 20:19:50 +0700

    fix missing attendee service

---
+++ src/core/service/AttendeeService.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include "../models/Attendee.hpp"
+#include "../models/Registration.hpp"
+#include "../Lists/AttendeeList.hpp"
+
+using namespace std;
+
+void registerAttendee(
+    Attendee *attendeeHead, 
+    Attendee *newAttendee
+) {
+    // ? Check if name exist
+
+    insertAttendee(attendeeHead, newAttendee);
+}
+
+void getAllAttendeesAndClassSessions(
+    Attendee *attendeeHead, 
+    Registration *registrationHead
+) {
+    Attendee *currentAttendee = attendeeHead;
+
+    if (attendeeHead == nullptr) {
+        cout << "No attendees available." << endl;
+        return;
+    }
+
+    while (currentAttendee != nullptr)
+    {   
+        cout << "Attendee: " << currentAttendee->data.name << " (ID: " << currentAttendee->data.id << ")" << endl;
+
+        Registration *currentRegistration = registrationHead;
+
+        cout << "Registered Class Sessions:" << endl;
+        while (currentRegistration != nullptr)
+        {
+            if (currentRegistration->attendee == currentAttendee)
+            {
+                cout << "- " << currentRegistration->class_session->name << endl 
+                << "Schedule: " << getDate(currentRegistration->class_session->schedule) << endl
+                << "Coach: " << currentRegistration->class_session->coach << endl
+                << " (Registration date: "
+                << getDate(currentRegistration->registration_date) << ")" << endl;
+            }
+            currentRegistration = currentRegistration->next;
+        }
+
+        currentAttendee = currentAttendee->next;
+    }
+}
+
+void countAttendeesClassSessions(
+    Attendee *attendeeHead, 
+    Registration *registrationHead
+) {
+    Attendee *currentAttendee = attendeeHead;
+
+    if (attendeeHead == nullptr) {
+        cout << "No attendees available." << endl;
+        return;
+    }
+
+    while (currentAttendee != nullptr)
+    {   
+        int count = 0;
+        Registration *currentRegistration = registrationHead;
+
+        while (currentRegistration != nullptr)
+        {
+            if (currentRegistration->attendee == currentAttendee)
+            {
+                count++;
+            }
+            currentRegistration = currentRegistration->next;
+        }
+
+        cout << "Attendee: " << currentAttendee->data.name << " is registered in " << count << " class session(s)." << endl;
+
+        currentAttendee = currentAttendee->next;
+    }
+}
